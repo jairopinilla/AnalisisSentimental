592db03a10a8caf6325f0f6e0f7b8cb5f7838de7
Jairo Pinilla <isaias1984@gmail.com>
2020-09-16 20:28:51 -0300

    gg

---
+++ app.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from pandas.plotting import register_matplotlib_converters
+from sklearn.model_selection import train_test_split
+import tensorflow_hub as hub
+import tensorflow_text 
