5eb1dd11e93cd5810d1ca7908a538ceba51c1fca
jairopinilla <isaias1984@gmail.com>
2020-09-16 22:01:33 -0300

    d

--- app.py
+++ app.py
@@ -11,3 +11,76 @@ from pandas.plotting import register_matplotlib_converters
 from sklearn.model_selection import train_test_split
 import tensorflow_hub as hub
 import tensorflow_text 
+#from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+from pathlib import Path
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+
+
+# Define a flask app
+app = Flask(__name__)
+
+
+path = Path("models/secuencial_model.h5")
+print(path)
+
+modelo = keras.models.load_model(path)
+use = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
+
+'''
+train_reviews_pred = ['tonto saco wea, eres un wn muy gil']
+for r in tqdm(train_reviews_pred):
+  emb = use(r)
+  review_emb = tf.reshape(emb, [-1]).numpy()
+
+review_emb=review_emb.reshape(1,512)
+y_pred = modelo.predict(review_emb)
+print(y_pred)
+"Bad" if np.argmax(y_pred) == 0 else "Good"
+'''
+#########################################
+def model_predict(frase):
+    train_reviews_pred = [frase]
+    
+    for r in tqdm(train_reviews_pred):
+        emb = use(r)
+        review_emb = tf.reshape(emb, [-1]).numpy()
+
+    review_emb=review_emb.reshape(1,512)
+    y_pred = modelo.predict(review_emb)
+
+    if (np.argmax(y_pred) == 0):
+        return "Negativo"
+    else:
+        return "Positivo"
+
+#########################################
+@app.route('/')
+def index():
+    # Main page
+    return "se levanta el servidor"
+
+#########################################
+@app.route('/predict', methods=['GET', 'POST'])
+def evaluacion():
+
+    content = request.get_json()
+    frase = content['frase']    
+    
+    print('el input es: ',frase)
+
+    retorno = model_predict(frase)
+    
+    return retorno
+   
+#########################################
+if __name__ == '__main__':
+    
+    app.run()
